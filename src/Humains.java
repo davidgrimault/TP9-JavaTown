f2df39aa533dc30e7659843c4531ff675dd9b7e5
AlexandreFeneux <alexandre.feneux@gmail.com>
2015-05-13 15:46:55 +0200

    Ajout de fonction Humains

--- src/Humains.java
+++ src/Humains.java
@@ -1,4 +1,22 @@
 
 public class Humains {
+	private String nom;
+	private String boissonFavorite ="eau";
+	
+	public Humains(String unNom ,String uneBoissonFavorite){
+		nom=unNom;
+		boissonFavorite = uneBoissonFavorite;
+	}
+	
+	public void parle(String text){
+		System.out.println(nom +" - "+ text);
+	}
 
+	public void presenter (){
+		System.err.println("Bonjour , je m'appelle "+ nom + " et moi boisson favorite est "+boissonFavorite+".");
+	}
+	
+	public void boire (){
+		System.out.println("Ah un bon verre de "+boissonFavorite+" ! GLOUPS !");
+	}
 }
