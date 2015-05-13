2c8cf1694eb1145d5baf3c354d5fdc43b22953ae
david91150 <davidgrimault.dg@gmail.com>
2015-05-13 15:50:39 +0200

    Ajout fonction class Humains

--- src/Humains.java
+++ src/Humains.java
@@ -1,4 +1,33 @@
 
 public class Humains {
+	private String nom;
+	private String boissonFavorite = "eau";
+
+	public Humains(String nom , String boissonFavorite) {
+		this.nom = nom ;
+		this.boissonFavorite = boissonFavorite;
+	}
+
+	public void parle( String texte) {
+		System.out.println(nom + " - " +texte);
+	}
+
+	public void presenter(){
+		System.out.println("Bonjour je m'apelle " + nom + " et ma boisson farovite est " + boissonFavorite + ".");
+	}
+	
+	public void boire(){
+		System.out.println("Ah un bon verre de " + boissonFavorite + " ! GLOUPS !");
+	}
+	public String quleEstTonNom(){
+		return nom;
+	}
+
+	
+
+
+
+
+
 
 }
